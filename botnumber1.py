5db997e3084a73c9da0c3b1f1e3100d527688df0
Dwellofhell <egornuta@gmail.com>
2023-04-19 16:26:29 +0300

    #добавил хэштеги

--- botnumber1.py
+++ botnumber1.py
@@ -1,28 +1,28 @@
 import requests
 import time
 
+API_URL: str = 'https://api.telegram.org/bot'  # ссылка на бота
+BOT_TOKEN: str = '6245798122:AAG9WztsZo4Flm9pKdT2NYQk8MZQQLDCWMw'  # ключик
+TEXT: str = 'ку'  # текст который отправится при любом сообщении
+MAX_COUNTER: int = 100  # количество восроизводства цикла
 
-API_URL: str = 'https://api.telegram.org/bot'                           #ссылка на бота
-BOT_TOKEN: str = '6245798122:AAG9WztsZo4Flm9pKdT2NYQk8MZQQLDCWMw'       #ключик
-TEXT: str = 'ку'            # текст который отправится при любом сообщении
-MAX_COUNTER: int = 100      # количество восроизводства цикла
-
-offset: int = -2
-counter: int = 0
+offset: int = -2  # самый новый апдейт
+counter: int = 0  # счетчик для взаимодействия с MAX_COUNTER чтобы программа не была вечно(пока что)
 chat_id: int
 
+while counter < MAX_COUNTER:  # если MAX_COUNTER == 0 то бот больше не отвечает
 
-while counter < MAX_COUNTER:
+    print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()  # перенос API в JSON
 
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
+    if updates['result']:  # начоло проверки на результат(апдейты)
         for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            offset = result['update_id']  # получаем номер апдейта(начиная с 1)
+            chat_id = result['message']['from']['id']  # номер чата из которого пришел апдейт
+            requests.get(
+                f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')  # отправка нужного сообщений я в
 
-    time.sleep(1)
+    time.sleep(1) # небольшая остоновка для лучшей работы сервера
     counter += 1
+
