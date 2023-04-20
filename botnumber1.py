3bba8ba43146b926ff06cf8f069d1d168bfa7b8d
Dwellofhell <egornuta@gmail.com>
2023-04-20 16:28:34 +0300

    дич

--- botnumber1.py
+++ botnumber1.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from pprint import pprint
 
 API_URL: str = 'https://api.telegram.org/bot'  # ссылка на бота
 BOT_TOKEN: str = '6245798122:AAG9WztsZo4Flm9pKdT2NYQk8MZQQLDCWMw'  # ключик
@@ -10,13 +11,20 @@ offset: int = -2  # самый новый апдейт
 counter: int = 0  # счетчик для взаимодействия с MAX_COUNTER чтобы программа не была вечно(пока что)
 chat_id: int
 
+
 while counter < MAX_COUNTER:  # если MAX_COUNTER == 0 то бот больше не отвечает
 
     print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
-
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()  # перенос API в JSON
+    updates2 = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    updates3 = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
-    if updates['result']:  # начоло проверки на результат(апдейты)
+
+    if updates['result']:
+        pprint(updates['result'][0]['update_id'])
+        pprint(updates2['result'][0]['update_id'])
+        pprint(updates3['result'][0]['update_id'])
+
         for result in updates['result']:
             offset = result['update_id']  # получаем номер апдейта(начиная с 1)
             chat_id = result['message']['from']['id']  # номер чата из которого пришел апдейт
@@ -25,4 +33,3 @@ while counter < MAX_COUNTER:  # если MAX_COUNTER == 0 то бот больш
 
     time.sleep(1) # небольшая остоновка для лучшей работы сервера
     counter += 1
-
