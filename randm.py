f6f17cc6963b5ad1b823b7cb04133ffdf52f1af5
Boyarishnik <boyarishnik3232@gmail.com>
2023-05-17 16:28:29 +0300

    #доработал диапазон

--- randm.py
+++ randm.py
@@ -1,12 +1,32 @@
 import random
+
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
+from aiogram.filters import BaseFilter
 from aiogram.filters import Text, Command
 from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
                            ReplyKeyboardRemove)
-from pprint import pprint
 
+# Этот фильтр будет проверять наличие неотрицательных чисел
+# в сообщении от пользователя, и передавать в хэндлер их список
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        # Разрезаем сообщение по пробелам, нормализуем каждую часть, удаляя
+        # лишние знаки препинания и невидимые символы, проверяем на то, что
+        # в таких словах только цифры, приводим к целым числам
+        # и добавляем их в список
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        # Если в списке есть числа - возвращаем список по ключу 'numbers'
+        if numbers:
+            return {'numbers': numbers}
+        return False
 
+first_digit = 1
+second_digit = 100
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
 BOT_TOKEN: str = '6245798122:AAG9WztsZo4Flm9pKdT2NYQk8MZQQLDCWMw'
@@ -22,30 +42,15 @@ button_2: KeyboardButton = KeyboardButton(text='Я не хочу играть!')
 button_3: KeyboardButton = KeyboardButton(text='Правила игры')
 button_4: KeyboardButton = KeyboardButton(text='Шутка')
 button_5: KeyboardButton = KeyboardButton(text='Cтатистика')
+button_6: KeyboardButton = KeyboardButton(text='Я хочу сменить диапазон угадываемых чисел')
 # Создаем объект клавиатуры, добавляя в него список списков с кнопками
 
-buttons: list[KeyboardButton] = []
-my_keyboard: list[list[KeyboardButton]] = []
-
-# Заполняем список списками с кнопками
 # Создаем объект клавиатуры, добавляя в него список списков с кнопками
 keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
                         keyboard=[[button_1, button_2, button_3],
-                                        [button_4,button_5]],
+                                        [button_4,button_5],
+                                        [button_6]],
                             resize_keyboard=True)
-
-for i in range(1, 101):
-    buttons.append(KeyboardButton(text=str(i)))
-    if not i % 8:
-        my_keyboard.append(buttons)
-        buttons = []
-    if i > 99:
-        my_keyboard.append(buttons)
-
-keyboard2: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-                        keyboard=my_keyboard,
-                            resize_keyboard=True)
-
 # Количество попыток, доступных пользователю в игре
 ATTEMPTS: int = 7
 
@@ -55,11 +60,6 @@ user: dict = {'in_game': False,
               'attempts': None,
               'total_games': 0,
               'wins': 0}
-
-
-# Функция возвращающая случайное целое число от 1 до 100
-def get_random_number() -> int:
-    return random.randint(1, 100)
 
 sp = ['У самой заботливой в мире девочки хомячок весит 22 килограмма.','Сколько раз бросал курить, но недалеко. Сигареты то лёгкие.','Карантин — как запой: выходить надо постепенно.',
       'Как называют человека, который продал свою печень? Обеспеченный','-Алло, это Чешская Республика? Почешите мне спинку.',
@@ -89,7 +89,7 @@ async def process_start_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Text(text='Правила игры'))
 async def process_help_command(message: Message):
-    await message.answer(f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
+    await message.answer(f'Правила игры:\n\nЯ загадываю число от {first_digit} до {second_digit}, '
                          f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
                          f'попыток\n\nДоступные команды: /cancel - выйти из игры\n'
                          f'\nДавай сыграем?')
@@ -101,6 +101,29 @@ async def process_stat_command(message: Message):
                          f'Игр выиграно: {user["wins"]}',
                          reply_markup=keyboard)
 
+# Смена диапазона
+@dp.message(Text(text='Я хочу сменить диапазон угадываемых чисел'))
+async def process_dip_command(message: Message):
+    await message.answer('Хорошо,ниже укажите через "-" от какого и до какого числа требуется диапазон)',
+                         reply_markup=keyboard)
+
+# От какого числа смена диапазона
+@dp.message(Text(contains='-', ignore_case=True))
+async def process_start_command(message: Message):
+    global first_digit
+    global second_digit
+    first_digit1 = message.text.split('-')
+    first_digit = first_digit1[0]
+    second_digit = first_digit1[1]
+    first_digit = int(first_digit)
+    second_digit = int(second_digit)
+    print(first_digit)
+    print(second_digit)
+    await message.answer('Вы успешно сменили диапазон!',
+                         reply_markup=keyboard)
+
+def get_random_number() -> int:
+    return random.randint(first_digit, second_digit)
 
 # Этот хэндлер будет срабатывать на команду "/cancel"
 @dp.message(Command(commands=['cancel']))
@@ -120,17 +143,16 @@ async def process_cancel_command(message: Message):
 @dp.message(Text(text=['Я хочу играть!'], ignore_case=True))
 async def process_positive_answer(message: Message):
     if not user['in_game']:
-        await message.answer('Ура!\n\nЯ загадал число от 1 до 100, '
-                             'попробуй угадать!',
-                             reply_markup=keyboard2)
+        await message.answer(f'Ура!\n\nЯ загадал число от {first_digit} до {second_digit} '
+                             f'попробуй угадать!')
 
         user['in_game'] = True
         user['secret_number'] = get_random_number()
         user['attempts'] = ATTEMPTS
     else:
-        await message.answer('Пока мы играем в игру я могу '
-                             'реагировать только на числа от 1 до 100 '
-                             'и команды /cancel и /stat')
+        await message.answer(f'Пока мы играем в игру я могу '
+                             f'реагировать только на числа от {first_digit} до {second_digit}'
+                             f'и команды /cancel и /stat')
 
 
 # Этот хэндлер будет срабатывать на отказ пользователя сыграть в игру
@@ -146,7 +168,7 @@ async def process_negative_answer(message: Message):
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем чисел от 1 до 100
-@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+@dp.message(lambda x: x.text and x.text.isdigit() and first_digit <= int(x.text) <= second_digit)
 async def process_numbers_answer(message: Message):
     if user['in_game']:
         if int(message.text) == user['secret_number']:
@@ -182,6 +204,6 @@ async def process_other_text_answers(message: Message):
     else:
         await message.answer('Я довольно ограниченный бот, давайте '
                              'просто сыграем в игру?')
-pprint(Message)
+
 if __name__ == '__main__':
     dp.run_polling(bot)
